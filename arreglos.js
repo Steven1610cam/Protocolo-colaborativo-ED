00fa4b4bc2bf4a1b1b62da39fc872e1cdefc44e3
DANIEL ARROYO <arroyosteven937@gmail.com>
2025-09-09 01:31:11 -0500

    feat(rama-arreglos): Declaracion y creacion del arreglo con 10 valores aleatorios

---
+++ arreglos.js
@@ -0,0 +1,9 @@
+let output = "\n--- Arreglos ---\n";
+
+    // 1.	Declaración y creación 
+    let numeros = new Array(10);
+
+    
+    for (let i = 0; i < numeros.length; i++) {
+        numeros[i] = Math.floor(Math.random() * 100);
+    }
